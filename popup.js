1ba3735f142276e72d0a24e340dfc60cd9d81619
Felix Thea <felixthea@gmail.com>
2014-05-03 18:49:03 -0400

    added log in and log out sequence

--- popup.js
+++ popup.js
@@ -3,39 +3,22 @@ API_BASE = 'http://localhost:3000'
 document.addEventListener('DOMContentLoaded', function () {
   var $logInForm = $('form#log-in');
 
-  if (savedSessionToken !== null ){
-    logInUsingSessionToken(savedSessionToken);
-    $logInForm.addClass('hidden')
-  }
+  if (savedSessionToken() !== null){
+    $logInForm.addClass('hidden');
+    $('button#log-out').removeClass('hidden');
+  } else {
+    $logInForm.removeClass('hidden');
+    $('button#log-out').addClass('hidden');
+  };
 
   $('form#log-in').on('click', '#submit', function(event){
-    var form = $('form#log-in');
-
     event.preventDefault();
+    logIn();
+  })
 
-    formData = form.serializeJSON();
-    $.ajax({
-      type: "POST",
-      url: API_BASE + "/session/ajax",
-      data: formData,
-      success: function(data,status,jqXHR){
-        console.log("console success")
-        console.log(data);
-        console.log(status);
-        console.log(jqXHR);
-        saveSessionToken(data["session_token"])
-        // $logInForm.addClass('hidden');
-        // $addWordsForm.removeClass('hidden');
-        // $('#welcome').html("Hi " + data["email"])
-        // $('button#log-out').removeClass('hidden');
-      },
-      error: function(jqXHR,textStatus,errorThrown){
-        console.log("console error");
-        console.log(jqXHR)
-        console.log(textStatus)
-        console.log(errorThrown)
-      }
-    })
+  $('button#log-out').on('click', function(event){
+    event.preventDefault();
+    logOut();
   })
 
   $('#stripe-customers-index').on('click', function(event){
@@ -97,6 +80,48 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log(response);
       });
     });
+  };
+
+  function logIn () {
+    var form = $('form#log-in');
+    formData = form.serializeJSON();
+    $.ajax({
+      type: "POST",
+      url: API_BASE + "/session/login",
+      data: formData,
+      success: function(data,status,jqXHR){
+        console.log("console success")
+        saveSessionToken(data["session_token"])
+        $logInForm.addClass('hidden');
+        $('button#log-out').removeClass('hidden');
+      },
+      error: function(jqXHR,textStatus,errorThrown){
+        console.log("console error");
+        console.log(jqXHR)
+        console.log(textStatus)
+        console.log(errorThrown)
+      }
+    });
+  };
+
+  function logOut () {
+    $.ajax({
+      type: "DELETE",
+      url: API_BASE + "/session/logout",
+      data: {"session_token": savedSessionToken()},
+      success: function(data,status,jqXHR){
+        console.log("logged out");
+        window.localStorage.removeItem('stripe-simple-cs');
+        $logInForm.removeClass('hidden');
+        $('button#log-out').addClass('hidden');
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+        console.log(jqXHR)
+        console.log(textStatus)
+        console.log(errorThrown)
+        console.log("error in logging out");
+      }
+    })
   }
 
 });
