ea387bf3a99eace28ba3c4337c1778c0387b4b19
Felix Thea <felixthea@gmail.com>
2014-05-04 15:24:14 -0400

    create dashboard and get customer email by looking for the email attribute

--- content.js
+++ content.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
+	$customerDashboard = $(
+		"<div id='customer-dashboard'> \
+			<h1>Customer Dashboard</h1> \
+			<div id='customer-info'></div> \
+		</div>"
+	)
+	$('body').prepend($customerDashboard);
+
 	window.setTimeout(function(){
-		var customerEmail = $('span.gD').attr('email');
+		var customerEmail = $('span[email]').attr('email');
 		var port = chrome.runtime.connect({name: "knock"});
 
-		port.postMessage({customer: customerEmail});
-		port.onMessage.addListener(function(msg){
-			console.log(msg);
+		port.postMessage({type: "retrieve_customer_with_charges", customerEmail: customerEmail});
+		port.onMessage.addListener(function(data){
+			// $('div#customer-info').html();
+			console.log(data);
 		});
 	}, 3000);
 	
